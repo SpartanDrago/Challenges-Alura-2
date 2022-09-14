28ec18d2bdcc301ae113b047088f6be30c2ecd6f
Spartan Drago <uni.joseluispita@gmail.com>
2022-09-13 22:17:06 -0500

    Funciones 50%

--- main.js
+++ main.js
@@ -1,38 +1,82 @@
-document.getElementById('btn-word').style.display = "none";
-document.getElementById('game').style.display = "none";
+/* LLamado */
+document.getElementById('box-word').style.display = "none";
+document.getElementById('box-game').style.display = "none";
 
-const IniciarJuego = document.querySelector("#go");
-const Rendirme = document.querySelector('#rendirse')
-const NuevaPalabra = document.querySelector("#add");
-const Medoy = document.querySelector("#rendirse");
-const cancel = document.querySelector("#cancelar")
+/* Botones */
+var iniciarJuego = document.getElementById('go');
+var jugar = document.getElementById('jugar');
+var addpal = document.getElementById('add');
+var btnguardaPalabra = document.getElementById('save');
 
-function Iniciar() {
-    document.getElementById('btn-init').style.display = "none";
-    document.getElementById('game').style.display = "";
+/* Funciones */
+function goJuego() {
+    document.getElementById('box-init').style.display = "none";
+    document.getElementById('box-game').style.display = "";
+
 }
 
-function rendirse() {
-    document.getElementById('game').style.display = "none";
-    document.getElementById('btn-init').style.display = "";
+function ventanaPalabra() {
+    document.getElementById('box-init').style.display = "none";
+    document.getElementById('box-game').style.display = "none";
+    document.getElementById('box-word').style.display = "";
 }
 
-function NewWord() {
-    document.getElementById('btn-word').style.display = "";
-    document.getElementById('btn-init').style.display = "none";
+function name() {
+    let addnewword = document.getElementById('inputAddPal').value;
+    console.log(addnewword);
 }
 
-function regresar() {
-    document.getElementById('game').style.display = "none";
-    document.getElementById('btn-init').style.display = "";
+function addDiv() {
+
+    for (let i = 0; i < 8; i++) {
+        $('.palabras').append(
+            $('<div>').prop({
+                id: 'innerdiv',
+                innerHTML: '<p>'+i+'</p>',
+                className: 'divpalabra'
+            })
+        );
+
+        $('.letras').append(
+            $('<div>').prop({
+                id: 'innerdiv',
+                innerHTML: '<h1>A</h1>',
+                className: 'divletra'
+            })
+        );
+    }
+
+    addpalabra();
+
 }
 
-function cancelar() {
-    document.getElementById('btn-word').style.display = "none";
-    document.getElementById('btn-init').style.display = "";
+function jugare() {
+    addDiv();
+    const btnjugar = document.getElementById('jugar');
+    btnjugar.disabled = true;
 }
 
-IniciarJuego.onclick = Iniciar;
-NuevaPalabra.onclick = NewWord;
-Medoy.onclick = regresar;
-cancel.onclick = cancelar;
+/*function names() {
+    let nombres = ["JOSE", "LUIS", "MANUEL", "CARMEN", "TERESA"];
+
+    var randon = Math.floor(Math.random()*nombres.length);
+    var send = nombres[randon];
+
+    console.log(send)
+}
+
+function addpalabra() {
+    let nombres = ["JOSE", "LUIS", "MANUEL", "CARMEN", "TERESA"];
+
+    nombres.push("PACO");
+    let datos = nombres[3];
+
+    console.log(datos);
+
+}*/
+
+/* Ejecucion */
+iniciarJuego.onclick = goJuego;
+addpal.onclick = ventanaPalabra;
+jugar.onclick = jugare;
+btnguardaPalabra.onclick = name;
